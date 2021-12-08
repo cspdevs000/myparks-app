ffe2ea93b216cceab12ba04c4c47dd32837a3657
cspdevs000 <cspdevs000@gmail.com>
2021-12-08 01:41:58 -0500

    got some data via axios

---
+++ controllers/home.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const passport = require("../config/ppConfig");
+
+const { User } = require('../models');
+
+module.exports = router;
--- scratchpad.js
+++ scratchpad.js
@@ -1,15 +1,32 @@
 const axios = require('axios');
-const API_KEY = "xGU2Bbmq7Bcwi6Oss2t0NTu1igEG_QwrwrysCiHvuxg";
+// const API_KEY = "xGU2Bbmq7Bcwi6Oss2t0NTu1igEG_QwrwrysCiHvuxg";
 
-const searchTerm = 'mexico';
+// const searchTerm = 'mexico';
 
-axios.get(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&&query=${searchTerm}`) // /users/:username/photos
-.then(response => {
-    // console.log(response.data);
-    const array = response.data.results;
-    for (let i = 0; i < array.length; i++) {
-        let photoObj = array[i];
-        let fullPhoto = photoObj.urls.full;
-        console.log(fullPhoto);
-    }
+// axios.get(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&&query=${searchTerm}`) // /users/:username/photos
+// .then(response => {
+//     // console.log(response.data);
+//     const array = response.data.results;
+//     for (let i = 0; i < array.length; i++) {
+//         let photoObj = array[i];
+//         let fullPhoto = photoObj.urls.full;
+//         console.log(fullPhoto);
+//     }
+// });
+
+let parkData = axios.get('https://developer.nps.gov/api/v1/parks?&api_key=H3HVNxmSbQNtQ6K9ek1o4qYpSJ27A8VaEiOAN32q').then (resp => {
+        // console.log(resp.data);
+        console.log(resp.data.data[1].fullName);
+        console.log(resp.data.data[1].designation);
+        console.log(resp.data.data[1].description);
+        console.log(resp.data.data[1].operatingHours);
+        console.log(resp.data.data[1].weatherInfo);
+        console.log(resp.data.data[1].images);
+        return resp.data.data[1].fullName;
 });
+
+console.log(parkData);
+
+module.exports = {
+    parkData
+}
