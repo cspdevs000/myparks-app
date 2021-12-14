de68bc41edfd6fcb67e20cf68bd4432e3acec8f8
cspdevs000 <cspdevs000@gmail.com>
2021-12-13 23:08:43 -0500

    seeding

--- seeders/20211214040241-demo-park.js
+++ seeders/20211214040241-demo-park.js
@@ -6,7 +6,7 @@ module.exports = {
     fullName: 'Acadia National Park',
     designation: 'National Park',
     description: 'Acadia National Park protects the natural beauty of the highest rocky headlands along the Atlantic coastline of the United States, an abundance of habitats, and a rich cultural heritage.',
-    waetherInfo: 'Located on Mount Desert Island in Maine, Acadia experiences all four seasons. Summer temperatures range from 45-90F (7-30C).',
+    weatherInfo: 'Located on Mount Desert Island in Maine, Acadia experiences all four seasons. Summer temperatures range from 45-90F (7-30C).',
     image: 'https://www.nps.gov/common/uploads/structured_data/3C7B45AE-1DD8-B71B-0B7EE131C7DFC2F5.jpg',
     createdAt: new Date(),
     updatedAt: new Date()
