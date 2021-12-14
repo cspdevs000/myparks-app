63563e9c38281861fa33a0509d713b188782f065
cspdevs000 <cspdevs000@gmail.com>
2021-12-13 23:33:26 -0500

    seeding

--- seeders/20211214040241-demo-park.js
+++ seeders/20211214040241-demo-park.js
@@ -3,14 +3,24 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
   await queryInterface.bulkInsert('Parks', [{
-    fullName: 'Arches National Park',
+    fullName: 'Badlands National Park',
     designation: 'National Park',
-    description: 'Visit Arches to discover a landscape of contrasting colors, land forms, and textures unlike any other in the world. The park has over 2,000 natural stone arches and hundreds of soaring pinnacles, massive rock fins, and giant balanced rocks.',
-    weatherInfo: "Arches is part of the Colorado Plateau, a 'high desert' region that experiences wide temperature fluctuations, sometimes over 40 degrees in a single day.",
-    image: 'https://www.nps.gov/common/uploads/structured_data/3C79850F-1DD8-B71B-0BC4A88BA85DE6B0.jpg',
+    description: 'The rugged beauty of the Badlands draws visitors from around the world. These striking geologic deposits contain one of the world’s richest fossil beds. Ancient horses and rhinos once roamed here.',
+    weatherInfo: "The Badlands weather is variable and unpredictable with temperature extremes ranging from 116° F to -40° F. Summers are hot and dry with occasional violent thunderstorms.",
+    image: 'https://www.nps.gov/common/uploads/structured_data/3C82EE63-1DD8-B71B-0BD6EE0FDCB5D402.jpg',
     createdAt: new Date(),
     updatedAt: new Date()
-   }])   
+   }, {
+    fullName: 'Big Bend National Park',
+    designation: 'National Park',
+    description: 'The night skies are dark as coal and rivers carve temple-like canyons in ancient limestone. Here, at the end of the road, hundreds of bird species take refuge in a solitary mountain range surrounded by weather-beaten desert.',
+    weatherInfo: "Big Bend is a desert park, so be prepared for extreme conditions.",
+    image: 'https://www.nps.gov/common/uploads/structured_data/8BF8356B-BB63-76A4-19F5296EF94C96B4.jpg',
+    createdAt: new Date(),
+    updatedAt: new Date()
+   }
+  
+  ])   
     /**
      * Add seed commands here.
      *
