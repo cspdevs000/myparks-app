6c13c4c6be5134b0ceefc60e4a3e9a4ee0314d64
cspdevs000 <cspdevs000@gmail.com>
2021-12-13 21:25:51 -0500

    testing

--- server.js
+++ server.js
@@ -18,10 +18,22 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+// app.use(session({
+//   secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+//   resave: false,             // Save the session even if it's modified, make this false
+//   saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+// }));
+
+app.set('trust proxy', 1);
+
 app.use(session({
-  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
-  resave: false,             // Save the session even if it's modified, make this false
-  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+cookie:{
+    secure: true,
+    maxAge:60000
+       },
+secret: SECRET_SESSION,
+saveUninitialized: true,
+resave: false
 }));
 
 app.use(flash());
