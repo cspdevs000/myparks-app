1ed759ebe9c47fcc9ba08396926c6a3bd4af4afb
cspdevs000 <cspdevs000@gmail.com>
2022-01-04 23:22:30 -0500

    fixed auth bug

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
-// app.use(session({
-//   secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
-//   resave: false,             // Save the session even if it's modified, make this false
-//   saveUninitialized: true    // If we have a new session, we save it, therefore making that true
-// }));
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
 
 app.set('trust proxy', 1);
 
